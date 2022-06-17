078c161fdf859c79c28005f3845733b29072e621
Francesco Ceccon <francesco@ceccon.me>
2022-06-17 13:28:57 +0100

    rofi

--- config/rofi.nix
+++ config/rofi.nix
@@ -3,13 +3,8 @@
 {
   programs.rofi = {
     enable = true;
-    font = "monospace 9";
     package =
       pkgs.rofi.override { plugins = [ pkgs.rofi-emoji pkgs.rofi-calc ]; };
-    extraConfig = {
-      fixed-num-lines = true;
-      case-sensitive = false;
-      matching = "fuzzy";
-    };
+    theme = ./rofi/theme.rasi;
   };
 }
