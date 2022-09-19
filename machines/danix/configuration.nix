10f3e155950bbedda4c22b546828945b6223353c
Francesco Ceccon <francesco@ceccon.me>
2022-09-19 14:35:32 +0100

    add danix back

---
+++ machines/danix/configuration.nix
@@ -0,0 +1,38 @@
+{ nixpkgs, pkgs, ... }:
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  fra.desktop = {
+    enable = true;
+    hostname = "t460p";
+  };
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  users.users.fra = { extraGroups = [ "i2c-dev" ]; };
+
+  # Enable logitech mouse.
+  hardware.logitech.wireless = {
+    enable = true;
+    enableGraphical = true;
+  };
+
+  # Enable vulkan.
+  hardware.opengl = {
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # WiFi
+  hardware.enableAllFirmware = true;
+  hardware.enableRedistributableFirmware = true;
+
+  programs.steam.enable = true;
+
+  programs.nix-ld.enable = true;
+
+  # Printer
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [ hplipWithPlugin ];
+  };
+}
