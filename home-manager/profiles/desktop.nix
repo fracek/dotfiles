ed2ca470188f5a21125133e7a130676b7670868d
Francesco Ceccon <francesco@ceccon.me>
2023-02-12 16:47:33 +0000

    default browser

--- home-manager/profiles/desktop.nix
+++ home-manager/profiles/desktop.nix
@@ -165,4 +165,19 @@
   };
 
   services.network-manager-applet = { enable = true; };
+
+  xdg = {
+    mime.enable = true;
+    mimeApps.defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+    };
+  };
+
+  home.sessionVariables = {
+    DEFAULT_BROSWER = "${pkgs.firefox}/bin/firefox";
+  };
 }
