959cd38f912b00240f4fe267c34a2a7e9551eaf0
Francesco Ceccon <francesco@ceccon.me>
2023-04-14 03:25:49 +0100

    mullvad vpn

--- machines/t14s/configuration.nix
+++ machines/t14s/configuration.nix
@@ -80,6 +80,12 @@
     enable = true;
   };
 
+  # vpn
+  services.mullvad-vpn = {
+    enable = true;
+    package = pkgs.mullvad-vpn;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
