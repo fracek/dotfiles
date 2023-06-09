cbc354faabaf322edb99e2779326e4cc6f00a3b1
Francesco Ceccon <francesco@ceccon.me>
2023-06-09 14:11:42 +0100

    tmux style

--- home-manager/modules/tmux/default.nix
+++ home-manager/modules/tmux/default.nix
@@ -24,7 +24,7 @@ in
 
       set-option -g message-style bg=#eba0ac,fg=#1e1e2e
 
-      set-window-option -g window-status-current-style fg=#b4befe
+      set-window-option -g window-status-current-style fg=#ebbcba
       set-window-option -g mouse on
       '';
     };
