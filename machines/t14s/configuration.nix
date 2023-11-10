04f938c6a8b5a0e4ee02ff4eaf6d662483277190
Francesco Ceccon <francesco@ceccon.me>
2023-11-10 14:22:49 +0100

    nix-ld on t14s

--- machines/t14s/configuration.nix
+++ machines/t14s/configuration.nix
@@ -84,6 +84,8 @@
 
   programs.steam.enable = true;
 
+  programs.nix-ld.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
