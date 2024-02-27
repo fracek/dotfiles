67a3be463eaa0c36f83add0784306e623df53728
Francesco Ceccon <francesco@ceccon.me>
2024-02-27 12:06:34 +0100

    wezterm opacity

--- home-manager/modules/wezterm/config/wezterm.lua
+++ home-manager/modules/wezterm/config/wezterm.lua
@@ -1,8 +1,13 @@
-return {
-  color_scheme = "Catppuccin Mocha",
-  font = require("wezterm").font("monospace"),
-  default_cursor_style = "BlinkingUnderline",
+local wezterm = require("wezterm")
 
-  window_close_confirmation = "NeverPrompt",
-  hide_tab_bar_if_only_one_tab = true,
-}
+local config = {}
+
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("monospace")
+config.default_cursor_style = "BlinkingUnderline"
+config.window_background_opacity = 0.95
+
+config.window_close_confirmation = "NeverPrompt"
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
