3e8af47526b5c9e2aab29a881e911db712af1de7
Francesco Ceccon <francesco@ceccon.me>
2024-06-27 12:36:25 +0200

    update tmux

--- home-manager/modules/tmux/default.nix
+++ home-manager/modules/tmux/default.nix
@@ -23,14 +23,14 @@ in
         set-option -g default-terminal "screen-256color"
 
         set-option -g status-position top
-        set-option -g status-style bg=default,fg=#cba6f7
+        set-option -g status-style bg=default,fg=#E0AF68
         set-option -g status-justify absolute-centre
         set-option -g status-left ""
         set-option -g status-right ""
 
-        set-option -g message-style bg=#f5c2e7,fg=#1e1e2e
+        set-option -g message-style bg=#9ECE6A,fg=#1A1B26
 
-        set-window-option -g window-status-current-style fg=#f5c2e7
+        set-window-option -g window-status-current-style fg=#9ECE6A
         set-window-option -g mouse on
       '';
     };
