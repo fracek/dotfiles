c85e47471faa02c5db5839b46dadb7031429cbc8
Francesco Ceccon <francesco@ceccon.me>
2024-12-29 15:17:10 +0100

    update

--- packages/morgen/default.nix
+++ packages/morgen/default.nix
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://dl.todesktop.com/210203cqcj00tw1/linux/deb/x64?v=${version}";
-    sha256 = "sha256-hvlsdaQ6M9Payisj6z5M2WtVbBmrm2Ct9SvR3muL9vw=";
+    sha256 = "sha256-kq7g79uKzk9k0RQ2OqHPRJxiHCYceVlfCUQ300rVn5E=";
   };
 
   nativeBuildInputs = [
