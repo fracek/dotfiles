e07dd579569908c6fa4df2256f450a39ef408069
Francesco Ceccon <francesco@ceccon.me>
2025-12-20 16:07:59 +0100

    disable cf dns

--- machines/t14s/configuration.nix
+++ machines/t14s/configuration.nix
@@ -82,7 +82,7 @@
     enable = true;
     dnssec = "true";
     domains = [ "~." ];
-    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    # fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
     dnsovertls = "true";
   };
 
