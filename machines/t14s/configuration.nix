57e8d8bf57c7b8ea5c2fb2c0bf316a73049b1bc4
Francesco Ceccon <francesco@ceccon.me>
2026-03-13 17:07:38 +0800

    disable dnssec

--- machines/t14s/configuration.nix
+++ machines/t14s/configuration.nix
@@ -83,10 +83,10 @@
 
   services.resolved = {
     enable = true;
-    dnssec = "true";
-    domains = [ "~." ];
-    # fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
-    dnsovertls = "true";
+    # dnssec = "true";
+    # domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    # dnsovertls = "true";
   };
 
   programs.steam.enable = true;
