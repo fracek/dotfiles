c07e09d586f269896ad95c599b39352e9558c13c
Francesco Ceccon <francesco@ceccon.me>
2026-07-12 21:13:15 +0200

    add t14s to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   danix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAIH585BI1nwwhzhoqHTUh2es8oNHKb2MzErN93hqiPI";
-  t14s = "";
+  t14s = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNsv4+5x5zMaRzqyqJo1Jgepmhuly3j5/h+GzDxWSZ/";
   nixnas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7bX5BtPt4d13KmpU/C/R4MlkQik6FlvhGXYfGpXlpx";
 
   all = [ danix nixnas ];
